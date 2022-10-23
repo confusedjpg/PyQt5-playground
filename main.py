0eda2655b9087b3b3850c61c32e96117ac146dc5
Dan <confused.jpg@pop-os.localdomain>
2022-10-23 18:56:18 +0200

    added QMessageBox() component example

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ class MainWindow(qtw.QWidget): #extend QWidget
         self.setWindowTitle("Dummy Window")
 
         self.label = qtw.QLabel("Hello There")
+        self.alert = qtw.QMessageBox(text="You clicked on that button I think.")
+        self.alert.setWindowTitle("AAAAAAAAAA")
         self.btn = qtw.QPushButton("I change the label.", clicked = lambda : self.label.setText('I got changed!'))
-
+        self.btn2 = qtw.QPushButton("I create an alert.", clicked = lambda : self.alert.exec())
         self.setLayout(qtw.QVBoxLayout())
         self.layout().addWidget(self.label)
         self.layout().addWidget(self.btn)
+        self.layout().addWidget(self.btn2)
 
         #show window
         self.show()
